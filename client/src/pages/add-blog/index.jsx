f402f2a028b103e4ef74bf4467316288ec6395bb
Ajinkya-kingre <145644231+Ajinkya-kingre@users.noreply.github.com>
2024-03-19 11:50:07 +0530

    successfully connect database,client and server

---
+++ client/src/pages/add-blog/index.jsx
@@ -0,0 +1,53 @@
+import { useContext } from "react";
+import classes from "./styles.module.css";
+import { GlobalContext } from "../../context";
+import axios from "axios";
+
+export default function AddNewBlog() {
+  const { formData, setFormData } = useContext(GlobalContext);
+  console.log(formData);
+
+  async function handleSaveDataToDatabase() {
+    const response = await axios.post("http://localhost:5000/api/blogs/add", {
+      title: formData.title,
+      description: formData.description,
+    });
+
+    const result = await response.data;
+    
+    console.log(result);
+  }
+  return (
+    <div className={classes.wrapper}>
+      <h1>Add A Blog</h1>
+      <div className={classes.formWrapper}>
+        <input
+          name="title"
+          placeholder="Enter Blog Title"
+          id="title"
+          type="text"
+          value={formData.title}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              title: e.target.value,
+            })
+          }
+        />
+        <textarea
+          name="description"
+          placeholder="Enter Blog Description"
+          id="description"
+          value={formData.description}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              description: e.target.value,
+            })
+          }
+        ></textarea>
+        <button onClick={handleSaveDataToDatabase}>Add New Blog</button>
+      </div>
+    </div>
+  );
+}
