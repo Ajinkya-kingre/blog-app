afa302deccbcbdaf2ee934e9548861cd51b2c887
Ajinkya-kingre <145644231+Ajinkya-kingre@users.noreply.github.com>
2024-04-13 15:09:24 +0530

    C:/Program Files/Git/

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require("./db");
 const app = express();
 app.use(
   cors({
-    origin: ["https://mern-f-blog-app.vercel.app/"],
+    origin: ["https://mern-f-blog-app.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
